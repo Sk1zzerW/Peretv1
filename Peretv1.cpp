ed4aa4d75b2588dc2f639ac411b9f25686bb4e16
olegk <kbryzhenyukpc@gmail.com>
2024-12-06 11:58:49 +0200

    Добавьте файлы проекта.

---
+++ Peretv1.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main() {
+    double distance, time;
+
+    std::cout << "введите расстояние до аеропорта (км) ";
+    std::cin >> distance;
+
+    std::cout << "введите время (час) ";
+    std::cin >> time;
+
+    double speed = distance / time;
+
+    std::cout << "вам нужно ехать  " << speed << " км/час." << std::endl;
+
+    return 0;
+}
